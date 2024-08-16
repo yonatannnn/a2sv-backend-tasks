0d90e5fe402540386eafbf3f5e8aae04ab89928b
yonatannnn <133119849+yonatannnn@users.noreply.github.com>
2024-08-16 10:32:40 +0300

    Update task_repository_test.go

--- task_manager_with_clean_architecture_test/Tests/task_repository_test.go
+++ task_manager_with_clean_architecture_test/Tests/task_repository_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	domain "api/task_manager/Domain"
-	repositories "api/task_manager/repository"
+	repositories "api/task_manager/Repository"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
